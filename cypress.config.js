00e098ee6d07838b8a72151cc93cb00cc2ce1c17
manindertuli126 <maninder.tuli126@gmail.com>
2024-04-16 17:48:37 -0400

    initia; commit 7

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,10 @@ const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
   e2e: {
-    specPattern:"**/*.feature",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("file:preprocessor", cucumber());
     },
+    specPattern:"**/*.feature",
   },
 });
